f43ef2e56e4e596c031472f221fcdac2a7586aff
Tomas alejandro Martinez cruz <tomartin@c1r9s5.42madrid.com>
2023-05-19 18:27:37 +0200

    go to home

--- src/decoder.c
+++ src/decoder.c
@@ -10,10 +10,25 @@ BN_gcd(gcd, a, b, ctx);
 BN_free(gcd);
 BN_CTX_free(ctx);
 */
+void decoder(t_corsair **s_data, int i) {
+    for (int j = i + 1; s_data[j] != NULL; ++j) {
+        if (s_data[j]->n == NULL || s_data[j]->n == 1) {
+            BIGNUM *gcd = BN_new();
+            BN_CTX *ctx = BN_CTX_new();
+            BN_gcd(gcd, s_data[i]->n, s_data[j]->n, ctx);
+            s_data[i]->p = BN_dup(gcd);
+            s_data[j]->p = BN_dup(gcd);
+            BN_CTX_free(ctx);
+            BN_free(gcd);
+        }
+    }
+}   
+
+
 void main_decoder(t_corsair **s_data) {
     int i = 0;
     while (s_data[i] != NULL) {
-        //decoder(s_data[i]);
+        decoder(s_data, i);
         ++i;
     }
 }

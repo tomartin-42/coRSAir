fe436fb393a34b40360ce011b45244fa90b04d04
Tomas alejandro Martinez cruz <tomartin@c1r2s6.42madrid.com>
2023-05-20 12:55:59 +0200

    nvim ok

--- src/corsair.c
+++ src/corsair.c
@@ -4,11 +4,11 @@ int main(int argc, char **argv) {
 
   t_corsair **s_data;
 
-  s_data = (t_corsair **)malloc(sizeof(t_corsair*) * argc);
+  s_data = (t_corsair **)malloc(sizeof(t_corsair *) * argc);
   s_data[argc] = NULL;
 
   for (int i = 1; i < argc; i++) {
-    s_data[i - 1] = (t_corsair*)malloc(sizeof(t_corsair));
+    s_data[i - 1] = (t_corsair *)malloc(sizeof(t_corsair));
     s_data[i - 1]->filename = argv[i];
     s_data[i - 1]->msg = ft_strdup(argv[i]);
     char *tmp = ft_strchr(s_data[i - 1]->msg, '.');
@@ -18,8 +18,8 @@ int main(int argc, char **argv) {
   }
 
   s_data[argc - 1] = NULL;
-  main_read_cert(s_data); 
+  main_read_cert(s_data);
+  printf("HOLA\n");
   main_decoder(s_data);
   print_s_ones(s_data);
-
 }

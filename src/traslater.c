9d1db2fc4c81cad9358cda1cea05a7f69f93b7c6
tomartin <tomartin@student.42madrid.com>
2023-06-04 12:02:37 +0200

    make a private key

--- src/traslater.c
+++ src/traslater.c
@@ -1,6 +1,7 @@
 #include "../inc/traslater.h"
+#include <openssl/rsa.h>
 
-void get_privateRSA(t_corsair *s_data) { ; }
+void get_privateRSA(t_corsair *s_data) { s_data->priv_rsa = RSA_new(); }
 
 void main_traslater(t_corsair **s_data) {
   for (int i = 0; s_data[i] != NULL; ++i) {
